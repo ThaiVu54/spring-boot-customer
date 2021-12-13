1d4ff5f78ed988cc0c51f16949ef4bfe9f56edd1
ThaiVu54 <thai01654369440@gmail.com>
2021-12-13 23:53:58 +0700

    customer spring boot

---
+++ src/main/java/com/example/customerspringboot/model/Customer.java
@@ -0,0 +1,11 @@
+package com.example.customerspringboot.model;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
